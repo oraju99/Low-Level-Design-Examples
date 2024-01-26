ea007930df65441a990d2a4cecb64edbc62e98bb
Rajeshwar Ojha <rajeshwar.ee17@iitp.ac.in>
2024-01-26 22:13:26 +0530

    Initial commit

---
+++ src/main/java/com/example/ParkingLot/ParkingLot.java
@@ -0,0 +1,13 @@
+package com.example.ParkingLot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingLot {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkingLot.class, args);
+        System.out.println("Working");
+    }
+}
