c837103e978852b6a3b6a23548c8527c49feb3fa
Rajeshwar Ojha <rajeshwar.ee17@iitp.ac.in>
2024-10-03 10:38:26 +0530

    Splitwise

--- src/main/java/com/example/Splitwise/Splitwise.java
+++ src/main/java/com/example/Splitwise/Splitwise.java
@@ -1,6 +1,5 @@
 package com.example.Splitwise;
 
-import com.example.ParkingLot.ParkingLot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
